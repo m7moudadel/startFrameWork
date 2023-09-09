20567df542c5836d565826febac6bf683f174a76
m7moudadel <ma7497093@gmail.com>
2023-09-09 21:37:54 +0300

    -startFrameWork

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  {path:'' , redirectTo:"home" , pathMatch:"full" , title:'Home'},
+  {path:'home' , component:HomeComponent, title:'Home'},
+  {path:'about' , component:AboutComponent ,title: 'About'},
+  {path:'portfolio' , component:PortfolioComponent, title: 'Portfolio'},
+  {path:'contact' , component:ContactComponent, title:'Contact'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
